588a8c42aff52b1ff85e0e35ec5aeaa2c5644cd0
Yi Yao <leaf_yao8621@hotmail.com>
2017-10-09 18:59:48 -0400

    modifications

--- src/test_app/main.c
+++ src/test_app/main.c
@@ -13,7 +13,7 @@ int handle(Engine* engine, Event* event) {
         sub(start, (Point*) get_data(event));
     }
     free_Point(get_data(event));
-    schedule_event(engine, get_time_stamp(event) + 3, 1, new_Point(1, 1));
+    schedule_event(engine, get_time_stamp(event) + 3, 2, new_Point(1, 1));
     printf("%d, %d\n", start->x, start->y);
     return 0;
 }
@@ -22,12 +22,15 @@ int main() {
     printf("%d, %d\n", start->x, start->y);
     engine = new_Engine(10);
     schedule_event(engine, 2.3, 1, new_Point(1, 1));
-    schedule_event(engine, 3, 2, new_Point(2, 2));
+
+    schedule_event(engine, 3, 1, new_Point(2, 2));
+
     schedule_event(engine, 2, 1, new_Point(3, 3));
     schedule_event(engine, 4, 1, new_Point(4, 4));
-    schedule_event(engine, 1, 1, new_Point(3, 3));
-    schedule_event(engine, 6, 2, new_Point(3, 3));
-    schedule_event(engine, 0.5, 1, new_Point(4, 4));
     schedule_event(engine, 3, 1, new_Point(1, 1));
+    schedule_event(engine, 1, 1, new_Point(3, 3));
+    schedule_event(engine, 6, 1, new_Point(3, 3));
+    schedule_event(engine, 0.5, 1, new_Point(4, 4));
+    schedule_event(engine, 1, 1, new_Point(5, 5));
     main_loop(engine);
 }
